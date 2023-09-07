84b09e1fe90bee18ce285536f0a8118aa4253fc2
Sumithra2004 <144336036+Sumithra2004@users.noreply.github.com>
2023-09-07 16:13:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# how to check leap year
+year=int(input(" Enter the year￼:"))
+
+if (year%400==0)and (year%100==0):
+   print(year,"is leap year")
+
+elif(year%4==0) and(year%100 !=0):
+  print(year,"is leap year")
+
+else:
+  print(year,"is leap year")
+  
+  
